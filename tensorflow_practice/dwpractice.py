53c26f06657a8b2bae108177ad8d4d506edf2765
stephenkais <stephenkais@gmail.com>
2016-11-08 11:40:11 -0500

    Add files via upload

---
+++ tensorflow_practice/dwpractice.py
@@ -0,0 +1,11 @@
+dwpractice.py
+#Deep Writing Practice
+#Stephen Kaiser
+
+#To start: Go to tensorflow_practice -> data -> tinyshakespeare -> "input.txt" to alter the text you would like to use deep writing with.
+#Open up terminal and run the following
+
+source ~/tensorflow/bin/activate
+cd ~/desktop/tensorflow_practice # Change to the location of this folder
+python train.py
+python sample.py
